838e7527367d4baa42eeeb3963d36a438a10c30e
Paul van der Walt <paul@denknerd.org>
2010-11-09 16:40:09 +0100

    Added greasemonkey script that resizes the Tasks pane in gCal to be more useful.

---
+++ larger_tasks.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name           larger tasks
+// @namespace      org.denknerd.gtasks
+// @description    makes the tasks pane in google calendar more usable
+// @include        http://www.google.com/calendar/*
+// ==/UserScript==
+//
+
+window.stretchTasks = function () {
+    divs = document.getElementsByTagName("div");
+    count = 0 ;
+    for(i = 0; i < divs.length; i++)
+    {
+        if(divs[i].className == 'sng-content')
+        {
+            count++;
+            indexWeWant = i; 
+            break;
+        }
+    }
+
+    //set the Tasks height:
+    divs[i].style.height = window.innerHeight - 260 + "px";
+}
+
+/* for Mozilla/Opera9 */
+if (document.addEventListener) {
+  document.addEventListener("load", stretchTasks, true);
+  //we also want this on windowresize:
+  window.addEventListener("resize", stretchTasks, true);
+}
+
